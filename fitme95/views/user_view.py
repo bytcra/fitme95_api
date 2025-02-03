87487f030dcc568d93cd988f12b140fa4a79e94e
Nitin-Singh18 <i.nitinsingh18@gmail.com>
2025-02-03 19:07:29 +0530

    fea: google sign in token verification

---
+++ fitme95/views/user_view.py
@@ -0,0 +1,44 @@
+from google.auth.transport import requests
+from google.oauth2 import id_token
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework import status
+from ..models.user import CustomUser
+from rest_framework.authtoken.models import Token
+
+
+@api_view(['POST'])
+@permission_classes([])
+def google_login(request):
+    try:
+        id_token_received = request.data.get('id')
+        if not id_token_received:
+            return Response({'error': 'ID Token is required'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Validate token with Firebase
+        google_info = id_token.verify_firebase_token(
+            id_token_received,
+            requests.Request(),
+        )
+
+        # User info
+        email = google_info.get('email')
+        google_id = google_info.get('sub')
+        name = google_info.get('name')
+
+        if not email:
+            return Response({'error': 'Invalid token, email not found'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Check if user already exists
+        user, created = CustomUser.objects.get_or_create(
+            google_id=google_id,
+            defaults={"name": name, "email": email}
+        )
+
+        # Generate authentication token
+        token, _ = Token.objects.get_or_create(user=user)
+
+        return Response({'message': 'Login successful', 'token': token.key, 'user': {'email': email, 'name': name}},
+                        status=status.HTTP_201_CREATED if created else status.HTTP_200_OK)
+    except Exception as e:
+        return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
