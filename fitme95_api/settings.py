31d8b1acd41821b4406ae25e8f725ecf7b50c8db
Nitin-Singh18 <i.nitinsingh18@gmail.com>
2025-02-26 13:12:12 +0530

    chore: add test database

--- fitme95_api/settings.py
+++ fitme95_api/settings.py
@@ -102,6 +102,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    # test database
+    'test': {
+        "NAME": BASE_DIR / 'test_db.sqlite3',
     }
 }
 
