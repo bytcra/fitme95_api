78a804359892dcf3af5d3925a65993c4091ab817
Nitin-Singh18 <i.nitinsingh18@gmail.com>
2025-03-11 12:08:09 +0530

    chore: update settings for secret key

--- fitme95_api/settings.py
+++ fitme95_api/settings.py
@@ -22,7 +22,7 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&v37wnppv8w1k-#dpcjag(su^2g)tnlo#)d2(+!_b81d4x6#06'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-&v37wnppv8w1k-#dpcjag(su^2g)tnlo#)d2(+!_b81d4x6#06')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
