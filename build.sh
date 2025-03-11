1c311645f5c0d8d6cbb86ed12fec25724366d9cc
Nitin-Singh18 <i.nitinsingh18@gmail.com>
2025-03-11 11:58:43 +0530

    chore: deploy setup

---
+++ build.sh
@@ -0,0 +1,7 @@
+# Exit on error
+set -o errexit
+
+pip install -r requirements.txt
+
+# Apply any outstanding database migrations
+python manage.py migrate
--- fitme95_api/settings.py
+++ fitme95_api/settings.py
@@ -9,12 +9,15 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
@@ -99,10 +102,8 @@ WSGI_APPLICATION = 'fitme95_api.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.getenv('DATABASE_URL'))
+
 }
 
 # Password validation
